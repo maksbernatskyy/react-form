83364ee25e8a30dc61b66d7e0e23c6a1526d1543
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-10 12:52:47 +0100

    add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 export default function Header() {
     return (
         <>
-        
+        <header>
+            <h1 className="text-uppercase p-3">
+                list of games
+            </h1>
+        </header>
         </>
     )
 }
