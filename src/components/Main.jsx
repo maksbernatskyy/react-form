bbff45708440d965981fe58f2f85f84d091793bc
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-10 14:11:48 +0100

    add input

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react"
+
 import list from "../assets/list"
 
 export default function Main() {
 
     <list />
+
+    const [titleGame, setTitleGame] = useState('')
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        console.log(`Il titolo è ${titleGame}`)
+    }
     
 
     return (
         <>
         <main>
             <div className="container text-center">
+                <div className="my-5">
+                    <form onSubmit={handleSubmit}>
+                        <input 
+                          type="text"
+                          value={titleGame}
+                          onChange={e => setTitleGame(e.target.value)} 
+                        />
+                        <button type="submit" className="ms-3">Invia</button>
+                    </form>
+                </div>
                 <div className="card text-start">
                     <ul>
                         {
